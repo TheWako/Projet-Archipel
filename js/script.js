56f2578929c32995d8283aea06b5001123ddd40b
TheWako <jean-philippe.isungu@supinternet.fr>
2017-01-19 19:46:33 +0100

    last update

--- js/script.js
+++ js/script.js
@@ -3,4 +3,42 @@ $(function () {
     $('#fullpage').fullpage({
         anchors: ['home', 'museum', 'application' ,'contact']
     });
+
+/*CAROUSEL*/
+    $('.carousel').slick({
+        infinite: true,
+        variableWidth: true,
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 3,
+        draggable: false,
+        arrows: false
+    });
+/*MODAL WINDOW*/
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 });
